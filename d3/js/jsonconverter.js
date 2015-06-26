9265fef3d1c950d9e68966f15ee5fdda036b4ab5
Julian Orth <julian1.orth@stud-uni.regensburg.de>
2015-06-26 14:56:40 +0200

    Visualisierung neu

---
+++ d3/js/jsonconverter.js
@@ -0,0 +1,18 @@
+function getAll() {
+    
+    
+ $.getJSON("./data/annual03.json", function(json){
+     
+     x = jsonsql.query("select * from json.journals", json);
+     
+     var count = x.length;
+     console.log("Anzahl Journals: ", count);
+
+ });
+    
+    
+}
+
+
+
+
